230057defbcf6cc540bb2fae87618e484e2f294f
Michael Schläpfer <schlaem91@gmail.com>
2022-11-15 08:21:50 +0100

    New extension methods for HttpClient and Streams.

--- Solution/SW.Framework/Extensions/HttpClientExtensions.cs
+++ Solution/SW.Framework/Extensions/HttpClientExtensions.cs
@@ -1,16 +1,52 @@
-﻿namespace SW.Framework.Extensions
-{
-    public static class HttpClientExtensions
-    {
-        public static async Task DownloadFileAsync(this HttpClient client, Uri uri, string filePath)
-        {
-            using (var stream = await client.GetStreamAsync(uri))
-            {
-                using (var fs = new FileStream(filePath, FileMode.Create))
-                {
-                    await stream.CopyToAsync(fs);
-                }
-            }
+﻿namespace SW.Framework.Extensions {
+  public static class HttpClientExtensions {
+    /// <summary>
+    /// Download from internet asynchronously.
+    /// </summary>
+    /// <remarks>
+    /// https://stackoverflow.com/questions/20661652/progress-bar-with-httpclient
+    /// </remarks>
+    /// <param name="client"></param>
+    /// <param name="requestUri"></param>
+    /// <param name="destination"></param>
+    /// <param name="progress"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public static async Task DownloadAsync(this HttpClient client, string requestUri, Stream destination, IProgress<float> progress = null, CancellationToken cancellationToken = default) {
+      // Get the http headers first to examine the content length
+      using (HttpResponseMessage response = await client.GetAsync(requestUri, HttpCompletionOption.ResponseHeadersRead)) {
+        long? contentLength = response.Content.Headers.ContentLength;
+
+        using (var download = await response.Content.ReadAsStreamAsync()) {
+          // Ignore progress reporting when no progress reporter was
+          // passed or when the content length is unknown
+          if (progress == null || !contentLength.HasValue) {
+            await download.CopyToAsync(destination, cancellationToken);
+            return;
+          }
+
+          // Convert absolute progress (bytes downloaded) into relative progress
+          Progress<long> relativeProgress = new Progress<long>(totalBytes => progress.Report((float)totalBytes / contentLength.Value));
+          // Use extension method to report progress while downloading
+          await download.CopyToAsync(destination, 81920, relativeProgress, cancellationToken);
+          progress.Report(1);
         }
+      }
     }
+
+    /// <summary>
+    /// Download a file asynchronously from internet.
+    /// </summary>
+    /// <param name="client"></param>
+    /// <param name="uri"></param>
+    /// <param name="filePath"></param>
+    /// <returns></returns>
+    public static async Task DownloadFileAsync(this HttpClient client, Uri uri, string filePath) {
+      using (Stream stream = await client.GetStreamAsync(uri)) {
+        using (FileStream fs = new(filePath, FileMode.Create)) {
+          await stream.CopyToAsync(fs);
+        }
+      }
+    }
+  }
 }
---
+++ Solution/SW.Framework/Extensions/StreamExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace SW.Framework.Extensions {
+  public static class StreamExtensions {
+    public static async Task CopyToAsync(this Stream source, Stream destination, int bufferSize, IProgress<long> progress = null, CancellationToken cancellationToken = default) {
+      if (source == null) {
+        throw new ArgumentNullException(nameof(source));
+      } else if (!source.CanRead) {
+        throw new ArgumentException("Has to be readable!", nameof(source));
+      } else if (destination == null) {
+        throw new ArgumentNullException(nameof(destination));
+      } else if (!destination.CanWrite) {
+        throw new ArgumentException("Has to be writable!", nameof(destination));
+      } else if (bufferSize < 0) {
+        throw new ArgumentOutOfRangeException(nameof(bufferSize));
+      }
+
+      byte[] buffer = new byte[bufferSize];
+      long totalBytesRead = 0;
+      int bytesRead;
+      while ((bytesRead = await source.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false)) != 0) {
+        await destination.WriteAsync(buffer, 0, bytesRead, cancellationToken).ConfigureAwait(false);
+        totalBytesRead += bytesRead;
+        progress?.Report(totalBytesRead);
+      }
+    }
+  }
+}
