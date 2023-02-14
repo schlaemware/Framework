82f22b86f7758f03858fdcfc4a7c6940e589c7ca
Michael Schläpfer <schlaem91@gmail.com>
2023-02-14 09:58:46 +0100

    Add extension method "Sum" for TimeSpans.

---
+++ Solution/SW.Framework/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace SW.Framework.Extensions {
+  public static class TimeSpanExtensions {
+    public static void Sum(this IEnumerable<TimeSpan> timeSpans) {
+      TimeSpan sum = TimeSpan.Zero;
+      foreach (TimeSpan span in timeSpans) {
+        sum += span;
+      }
+
+      return sum;
+    }
+  }
+}
