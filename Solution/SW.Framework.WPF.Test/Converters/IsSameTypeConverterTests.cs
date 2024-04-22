f2b4982e306e7a284a348445903c69899c0ebcd6
Michael Schläpfer <michael.schlaepfer@neovac.ch>
2024-04-22 12:58:45 +0200

    New converter "IsSameTypeConverter" with corresponding tests implemented.

---
+++ Solution/SW.Framework.WPF.Test/Converters/IsSameTypeConverterTests.cs
@@ -0,0 +1,20 @@
+﻿using SW.Framework.WPF.Converters;
+
+namespace SW.Framework.WPF.Test.Converters
+{
+    [TestClass]
+    public class IsSameTypeConverterTests
+    {
+        [TestMethod]
+        [DataRow("hello", typeof(string), true)]
+        [DataRow("hello", typeof(int), false)]
+        public void ConversionTest(object input, Type referenceType, bool result)
+        {
+            IsSameTypeConverter converter = new();
+
+            object output = converter.Convert(input, typeof(bool), referenceType, System.Globalization.CultureInfo.CurrentCulture);
+
+            Assert.AreEqual(output, result);
+        }
+    }
+}
