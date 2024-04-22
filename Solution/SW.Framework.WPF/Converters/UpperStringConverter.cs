b3ae088fa8c2242e74e0ec56e71de3a6f2a8a7a1
Michael Schläpfer <michael.schlaepfer@neovac.ch>
2024-04-22 08:09:22 +0200

    New package "SW.Framework.WPF" with converter "UpperStringConverter" and corresponding tests added.

---
+++ Solution/SW.Framework.WPF/Converters/UpperStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace SW.Framework.WPF.Converters
+{
+    [ValueConversion(typeof(string), typeof(string))]
+    public class UpperStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string input)
+            {
+                return input.ToUpper();
+            }
+
+            throw new ArgumentException($"input must be a string vlaue, not {value.GetType()}");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
