a043a1fda0fd77fc590ce6647c46f18e01efb084
Michael Schläpfer <michael.schlaepfer@neovac.ch>
2024-05-08 09:14:07 +0200

    New converter 'VisibleIfNotNullConverter' added.

---
+++ Solution/SW.Framework.WPF/Converters/VisibleIfNotNullConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SW.Framework.WPF.Converters
+{
+    [ValueConversion(typeof(object), typeof(Visibility))]
+    public class VisibleIfNotNullConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
